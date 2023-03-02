29f8fe86fa9f9f1bf0e8802f88236edf6521d404
son_pop <65592074+chrismahony@users.noreply.github.com>
2023-03-02 10:25:21 +0000

    Update check_propotions_of_cells.R

--- check_propotions_of_cells.R
+++ check_propotions_of_cells.R
@@ -1,37 +1,27 @@
-obj$sample_condition<-paste(obj$sample, obj$condition, sep=".") #condition would be resting/peak/resolved etc
+library(tidyr)
+obj$sample_condition<-paste(obj$orig.ident, obj$condition, sep=".") #condition would be resting/peak/resolved etc
 
 pt <- table(obj$sample_condition, obj$cluster.name)
 pt <- as.data.frame(pt)
 
+pt<-pt %>%
+  pivot_wider(names_from = Var2, values_from = Freq) %>% 
+  as.data.frame() 
+
 library(tidyverse)
-pt_cluster1 = subset(pt, Var2 == "cluster1")
-pt_cluster2 = subset(pt, Var2 == "cluster2")
-pt_cluster3 = subset(pt, Var2 == "cluster3")
+pt <- pt %>% remove_rownames %>% column_to_rownames(var="Var1")
+pt <- pt/rowSums(pt)
 
-colnames(pt_cluster1) <- c("Var1", "Var2", "cluster1")
-colnames(pt_cluster2)<- c("Var1", "Var2", "cluster2")
-colnames(pt_cluster3)<- c("Var1", "Var2", "cluster3")
-
-pt_master<-pt_cluster1
-pt_master$cluster2<-cluster2$cluster2
-pt_master$cluster3<-cluster3$cluster3
-
-pt_master <- pt_master %>% select(-one_of('Var2'))
-library(tidyverse)
-pt_master <- pt_master %>% remove_rownames %>% column_to_rownames(var="Var1")
-pt_master <- pt_master/rowSums(pt_master)
-pt_master <- as.data.frame(pt_master)
-
-pt_master$condition<-rownames(pt_master)
+pt$condition<-rownames(pt)
 
 library(splitstackshape)
-pt_master<-cSplit(pt_master, splitCols="condition", sep=".")
+pt<-cSplit(pt, splitCols="condition", sep=".")
 
-#chnage the levels for what you have i your object. They will apear in the order they are written
-cluster1 <- ggplot(pt_master, aes(x=factor(condition, level=c('control', 'initiation', 'peak', 'resolving', 'resolved', 'persistant')), y=cluster1)) + 
-  geom_violin() + geom_dotplot(binaxis='y', stackdir='center', dotsize=1)+ggtitle("Chodl")+theme_classic()+RotatedAxis()
+
+cluster1 <- ggplot(pt, aes(x=factor(condition_2, level=c('control', 'initiation', 'peak', 'resolving', 'resolved', 'persistant')), y=cluster1)) + 
+  geom_violin() + geom_dotplot(binaxis='y', stackdir='center', dotsize=1)+ggtitle("Cluster1")+theme_classic()+RotatedAxis()
 cluster1
-res.aov_cluster1 <- aov(cluster1 ~ condition, data = pt_master)
+res.aov_cluster1 <- aov(cluster1` ~ condition_2, data = pt)
 summary(res.aov_cluster1)
 stats_cluster1 <- TukeyHSD(res.aov_cluster1)
 stats_cluster1
