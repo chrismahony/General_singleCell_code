d9ff5da19a472cd656707fac292de27069f6a3f2
mahonyc <65592074+chrismahony@users.noreply.github.com>
2024-05-03 11:42:37 +0100

    Update write_out_seurat_obj.R

--- write_out_seurat_obj.R
+++ write_out_seurat_obj.R
@@ -1,2 +1,4 @@
 library(DropletUtils)
+#folder must not already exist i.e. you are creating a new one
 write10xCounts(path="/path/", x=obj@assays$RNA@counts)
+write.table(obj@meta.data %>% as.data.frame(), "/path/meta.tsv", sep="\t")
