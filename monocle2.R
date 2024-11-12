bc5f7453ac37f10a069577fc65c8368d144a197c
mahonyc <65592074+chrismahony@users.noreply.github.com>
2024-11-12 09:41:55 +0000

    Update monocle2.R

--- monocle2.R
+++ monocle2.R
@@ -10,6 +10,8 @@ Idents(fibs)<-'endo_sub.cluster.0.1'
 
 all_markers_fibs<-FindAllMarkers(fibs, only.pos = T)
 
+
+
 all_markers_fibs %>%
     group_by(cluster) %>%
     top_n(n = 50, wt = avg_log2FC) -> all_markers_fibs_top50
@@ -56,3 +58,49 @@ rna_fibros_g2_rev <- orderCells(rna_fibros_g2, reverse = T)
 plot_cell_trajectory(rna_fibros_g2, color_by = "endo_sub.cluster.0.1", cell_size = 1,show_branch_points = F)
 plot_cell_trajectory(rna_fibros_g2, color_by = "TYPE", cell_size = 1,show_branch_points = F)
 plot_cell_trajectory(rna_fibros_g2, color_by = "Pseudotime", cell_size = 1,show_branch_points = F)
+
+
+
+
+
+####### interating over the top n markers to find the best braching ######
+na_fibros_g2  <- estimateSizeFactors(rna_fibros_g2)
+
+
+markers_list <- list()
+monocle_plots <- list()
+
+top_n <- c(10,20,50,100,200,300)
+
+
+for (i in 1:lenght(top_n)){
+  
+  markers_list[[i]] <- all_markers_fibs %>%
+    group_by(cluster) %>%
+    top_n(n = top_n[[i]], wt = avg_log2FC)
+  
+
+
+rna_fibros_g2 <- setOrderingFilter(rna_fibros_g2, markers_list[[i]]$gene)
+rna_fibros_g2 <- reduceDimension(rna_fibros_g2, max_components = 2, method = 'DDRTree')
+rna_fibros_g2 <- orderCells(rna_fibros_g2)
+rna_fibros_g2_rev <- orderCells(rna_fibros_g2, reverse = T) #adjust to meta.data slots as needed
+monocle_plots[[i]] <- plot_cell_trajectory(rna_fibros_g2, color_by = "endo_sub.cluster.0.1", cell_size = 1,show_branch_points = F) + ggtitle(paste(i, "marker genes")) #adjust to meta.data slots as needed
+monocle_plots[[i]] <- plot_cell_trajectory(rna_fibros_g2, color_by = "TYPE", cell_size = 1,show_branch_points = F) + ggtitle(paste(i, "marker genes"))#adjust to meta.data slots as needed
+monocle_plots[[i]] <- plot_cell_trajectory(rna_fibros_g2, color_by = "Pseudotime", cell_size = 1,show_branch_points = F)+ ggtitle(paste(i, "marker genes"))
+
+}
+
+
+#plot again as required
+
+for (i in 1:lenght(monocle_plots)){
+  
+  print(monocle_plots[[i]])
+}
+
+
+#choose the best number of genes, rebeat the ordering with those genes and then you have your final obj
+
+
+
