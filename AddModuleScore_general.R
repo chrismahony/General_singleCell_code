03d5d199ac7d39bdd818eb8aa096ec59f73afbef
mahonyc <65592074+chrismahony@users.noreply.github.com>
2024-11-18 16:20:47 +0000

    Update AddModuleScore_general.R

--- AddModuleScore_general.R
+++ AddModuleScore_general.R
@@ -5,7 +5,7 @@ library(Seurat)
 df_f <- subres %>% filter(log2FoldChange > 1 & padj < 0.05)
 
 #if multiple comparisons have been made
-df_f <- subres %>% filter(comparison == "Dex_vs_LPS" & log2FoldChange > 1 & padj < 0.05)
+df_f <- subres %>% filter(comparison == "Dex_vs_LPS" & log2FoldChange > 1 & padj < 0.05)  #adulst cot off as required, you will need to try different ones
 
 ### !! make sure you take the correct log2FOldChnage direcetion. log2FoldChange > 1 in Dex_vs_LPS are gene increased in Dex ###
 
